3cd65abea498669ebb37dce8e969369a3979805e
Zin Min Htet <zinminn34119@gmail.com>
2022-09-02 11:32:20 +0630

    add input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// class Item extends React.Component {
+//   render(){
+//     return <li>Zin</li>
+//   }
+// }
+
+// class App extends React.Component {
+//   render(){
+//     return (
+//       <div>
+//         <h1>Hello React</h1>
+//         <ul>
+//             <Item />
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+
+// class App extends React.Component {
+//   render(){
+//     return (
+//       <div>
+//         <h1>Hello React</h1>
+//         <ul>
+//           <Item name="apple" price="500 ks"/>
+//           <Item name="orange" price="600 ks"/>
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+
+class Item extends React.Component {
+  render(){
+    return (
+      <div>
+        <li>
+        {this.props.id}, {this.props.name}, {this.props.price},
+        </li>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  state = {
+    items : [
+      {id:1,name:'apple',price:1050},
+      {id:2,name:'orange',price:20},
+    ]
+  }
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  
+  add = () => {
+    let id = this.state.items.length + 1;
+    let name = this.nameRef.current.value;
+    let price = this.priceRef.current.value;
+
+    this.setState({
+      items : [
+        ...this.state.items,
+        {id, name, price}
+      ]
+    })
+  }
+  render(){
+    return(
+      <div>
+        <h1>Hello React</h1>
+        <input type="text" ref={this.nameRef} /><br/>
+        <input type="text" ref={this.priceRef} />
+        <button onClick={this.add}>Add</button>
+        <ul>
+          {this.state.items.map(i => {
+            return (
+              <Item 
+              key={i.id}
+              id={i.id}
+              name={i.name} 
+              price={i.price}
+              />
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
+    
---
+++ src/components/Greet.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// function Greet(){
+//     return <h1>Hello Zin Min</h1>
+// }
+const Greet = ()=> <h1>ES6 function</h1>
+
+export default Greet
