cc8847616d14df203d5682e8fc1c7b0b5efef277
Zin Min Htet <zinminn34119@gmail.com>
2022-09-04 16:53:38 +0630

    test sass

--- src/App.js
+++ src/App.js
@@ -1,96 +1,40 @@
 import React from "react";
-
-
-// class Item extends React.Component {
-//   render(){
-//     return <li>Zin</li>
-//   }
-// }
-
-// class App extends React.Component {
-//   render(){
-//     return (
-//       <div>
-//         <h1>Hello React</h1>
-//         <ul>
-//             <Item />
-//         </ul>
-//       </div>
-//     )
-//   }
-// }
-
-// class App extends React.Component {
-//   render(){
-//     return (
-//       <div>
-//         <h1>Hello React</h1>
-//         <ul>
-//           <Item name="apple" price="500 ks"/>
-//           <Item name="orange" price="600 ks"/>
-//         </ul>
-//       </div>
-//     )
-//   }
-// }
-
-class Item extends React.Component {
-  render(){
-    return (
-      <div>
-        <li>
-        {this.props.id}, {this.props.name}, {this.props.price},
-        </li>
-      </div>
-    )
-  }
-}
+import Navbar from "./Navbar";
+import "./cus/css/cus.css"
 
 class App extends React.Component {
-  state = {
-    items : [
-      {id:1,name:'apple',price:1050},
-      {id:2,name:'orange',price:20},
-    ]
-  }
-  nameRef = React.createRef();
-  priceRef = React.createRef();
-  
-  add = () => {
-    let id = this.state.items.length + 1;
-    let name = this.nameRef.current.value;
-    let price = this.priceRef.current.value;
-
-    this.setState({
-      items : [
-        ...this.state.items,
-        {id, name, price}
-      ]
-    })
-  }
-  render(){
-    return(
+  render (){
+    let styles = {
+      bg : {
+        backgroundColor: 'red',
+        color: 'white',
+        padding : 20,
+      }
+    }
+    return (
       <div>
-        <h1>Hello React</h1>
-        <input type="text" ref={this.nameRef} /><br/>
-        <input type="text" ref={this.priceRef} />
-        <button onClick={this.add}>Add</button>
-        <ul>
-          {this.state.items.map(i => {
-            return (
-              <Item 
-              key={i.id}
-              id={i.id}
-              name={i.name} 
-              price={i.price}
-              />
-            )
-          })}
-        </ul>
+        
+        <Navbar>
+          Navication
+        </Navbar>
+
+        {/* CSS Modules End */}
+
+        <h1 style={styles.bg} >CSS in JS</h1>
+
+        {/* Css inline End */}
+
+        <div className="background" >
+          <h1>Logo & GG</h1>
+          <p>This is paragraph...</p>
+          <button className="btn" >Click</button>
+        </div>
+
       </div>
     )
   }
 }
 
+
 export default App;
-    
+        
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -3,6 +3,41 @@ import React from "react";
 // function Greet(){
 //     return <h1>Hello Zin Min</h1>
 // }
-const Greet = ()=> <h1>ES6 function</h1>
+// const Greet = ()=> <h1>ES6 function</h1>
 
-export default Greet
+// export default Greet
+
+class Nav extends React.Component {
+    render(){
+        return (
+            <div>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+class Second extends React.Component {
+    render(){
+        return (
+            <div>
+                {this.props.children }
+            </div>
+        )
+    }
+}
+class Final extends React.Component {
+    render(){
+        return (
+            <div>
+                <Nav>
+                    <h1 style={{background : "black",color : "white", padding : 20 }} >Navication</h1>
+                </Nav>
+                <Second>
+                    <h1>Hello Testing</h1>
+                    <p>Component Composition</p>
+                </Second>
+            </div>
+        )
+    }
+}
+export default Final
